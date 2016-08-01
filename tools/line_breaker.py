0e6bf6746e82d1b22a85f81f67021bf3445feb73
tarmo <tarmo@tarmo.org>
2016-08-01 21:45:48 +0900

    0801-2 &241 test

--- tools/line_breaker.py
+++ tools/line_breaker.py
@@ -11,23 +11,26 @@ def clear_escape(s: str):
 def word_cnt(s: str):
     cnt = 0
     s = clear_escape(s)
-    if all([x.isalnum() or x.isspace() or x in ["-",".","'",'%',',','<','>'] for x in s]):
+    if not any([i.isalpha() and not (i.islower() or i.isupper()) for i in s]):
         return 0
     for i in s:
-        if i.isalnum() or i.isspace():
-            cnt += 0.5
-        else:
+        if i.isalpha() and not (i.islower() or i.isupper()):
             cnt += 1
+        else:
+            cnt += 0.5
     return cnt
 for line in ifile:
     if line.startswith('Ubertip='):
-        target = line[9:-2]
+        fq = line.find('"')
+        lq = line.rfind('"')
+        target = line[fq + 1: lq]
         splited = target.split('|n')
         for i in range(len(splited)):
             if word_cnt(splited[i]) > threshold:
-                if ' ' in splited[i] and not ('作者' in splited[i] or '模组' in splited[i] ):
+                if ' ' in splited[i] and not ('作者' in splited[i] or '模组' in splited[i]):
                     splited[i] = splited[i].replace(' ','')
-        line = line[:9] + '|n'.join(splited) + line[-2:]
+                    print(splited[i])
+        line = line[:fq + 1] + '|n'.join(splited) + line[lq:]
 
     ofile.write(line)
 
