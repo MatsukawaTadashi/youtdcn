e733073c59a8fab7e050ade76ea8c04e903437ea
tarmo <tarmo@tarmo.org>
2017-01-14 23:18:29 +0900

    tower translated! items builders pending...

--- tools/write_back.py
+++ tools/write_back.py
@@ -7,12 +7,18 @@ parser = argparse.ArgumentParser()
 # parser.add_argument('-i','--input',type=str,required=True,help='The input file')
 args = parser.parse_args()
 args.base = r'../map'
-args.input = r'cn.txt'
+args.input = r'tl.txt'
 
 ytd = War3Map(args.base)
 txt = UnitTextFile(args.input)
 for unit in txt.unit_list:
-    ytd.unit_collection[unit.id].string.text = unit.text
+    st = ytd.unit_collection[unit.id].string_text
+    print(unit.id)
+    for attr in unit.attr_dict:
+        print(attr)
+        assert attr in st.attr_dict
+        st.attr_dict[attr][1] = unit.attr_dict[attr][1]
 
 for prefix in ytd.unit_text_file_collection:
-    ytd.unit_text_file_collection[prefix][0].write_back()
+    string, func = ytd.unit_text_file_collection[prefix]
+    string.write_back()
